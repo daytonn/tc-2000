6e29135849354baaf72bb6ef15908c0f3f40be6f
Dayton Nolan <me@daytonnolan.com>
2024-09-07 21:56:42 -0700

    Add interactive mode, move files into index.html

--- build.sh
+++ build.sh
@@ -1,11 +1,1 @@
 #!/usr/bin/env bash
-
-rm -rf public
-mkdir public
-mkdir public/js
-
-cp index.html public/index.html
-cp -R styles public/
-cp -R svg public/
-cp -R images public/
-cp js/bundle.js public/js/bundle.js
